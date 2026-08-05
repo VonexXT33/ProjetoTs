4e6533140aab72bc1220f0637c5f87c67a3c8594
VonexXT33 <josealv.neto04b@gmail.com>
2026-08-04 21:51:57 -0300

    Array.js adicionado

---
+++ output/array.js
@@ -0,0 +1,7 @@
+"use strict";
+const filmesCategorias = ["Ação", "Comédia", "Drama", "Ficção Científica", "Terror"];
+const acessorios = ["Mouse", "Teclado", "Monitor", "Headset"];
+acessorios.push("Webcam");
+acessorios.push("Microfone");
+console.log(acessorios);
+//# sourceMappingURL=array.js.map
