bbfaed6358059707f658af44db4b959eeb247e03
VonexXT33 <josealv.neto04b@gmail.com>
2026-08-05 12:16:04 -0300

    Object.ts adicionado

---
+++ output/object.js
@@ -0,0 +1,8 @@
+"use strict";
+let configuracaoServidor = {
+    servidor: 'Microsoft Azure',
+    ip: '127.0.0.1',
+    senha: '654321'
+};
+console.log(configuracaoServidor);
+//# sourceMappingURL=object.js.map
---
+++ scripts/object.ts
@@ -0,0 +1,16 @@
+let configuracaoServidor: object = {
+    servidor: 'Microsoft Azure',
+    ip: '127.0.0.1',
+    senha: '654321'
+}
+
+console.log(configuracaoServidor);
+
+
+// configuracaoServidor = {
+//     servidor: 'Amazon Web Services',
+//     ip: '192.168.1.1',
+//     senha: '123456'
+// }
+
+//      console.log(configuracaoServidor);
