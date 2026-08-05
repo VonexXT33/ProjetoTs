3051f6151f4c2188ca2a2555cb82cf6f85a23c3f
VonexXT33 <josealv.neto04b@gmail.com>
2026-08-05 00:09:00 -0300

    Tuple.ts adicionado

---
+++ output/tuple.js
@@ -0,0 +1,5 @@
+"use strict";
+let diasSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+diasSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+console.log(diasSemana);
+//# sourceMappingURL=tuple.js.map
