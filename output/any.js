876e5a557e4416d96172bdb2f40d6acc57a3fc96
VonexXT33 <josealv.neto04b@gmail.com>
2026-08-06 00:07:30 -0300

    Any.ts adicionado

---
+++ output/any.js
@@ -0,0 +1,10 @@
+"use strict";
+let cadastro1 = {
+    nome: 'José',
+    idade: 24,
+    cidade: 'Narandiba.SP',
+    profissao: 'Programador',
+    salario: 2500.00
+};
+console.log(cadastro1);
+//# sourceMappingURL=any.js.map
---
+++ scripts/any.ts
@@ -0,0 +1,21 @@
+let cadastro1: { nome: string; idade: number; cidade: string; profissao: string; salario: number } = {
+    nome: 'José',
+    idade: 24,
+    cidade: 'Narandiba.SP',
+    profissao: 'Programador',
+    salario: 2500.00
+};
+
+
+
+
+// let cadastro2: any;
+
+// cadastro2.nome = 'Maria';
+// cadastro2.idade = 30;
+// cadastro2.cidade = 'São Paulo.SP';
+// cadastro2.profissao = 'Analista de Sistemas';
+// cadastro2.salario = 3500.00;
+
+console.log(cadastro1); 
+
