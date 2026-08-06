b4b3de41b295f9e65622ed79a51a0046efb24c8c
José Alves <josealv.neto04b@gmail.com>
2026-08-06 16:23:50 -0300

    Interface.js adiicionada

---
+++ output/interface_prop_opicional.js
@@ -0,0 +1,8 @@
+"use strict";
+const produtoDados = {
+    nome: 'Xbox series s',
+    preco: 2200,
+    dataValidade: new Date(2026, 11, 22)
+};
+console.log(produtoDados);
+//# sourceMappingURL=interface_prop_opicional.js.map
