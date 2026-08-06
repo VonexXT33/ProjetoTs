1e55075e265848dbf3cb8b3235b20c7fdcd30a4c
VonexXT33 <josealv.neto04b@gmail.com>
2026-08-05 23:28:19 -0300

    Null_undefined.ts adicionado

---
+++ scripts/null_undefined.ts
@@ -0,0 +1,15 @@
+let db: string | null = 'mysql, 127.0.0.1, password';
+
+db = null;
+
+// console.log(db);
+
+let minhaVariavel: string | undefined;
+
+if (new Date().getDay()== 1) {
+
+    minhaVariavel = 'Quarta-feira';
+
+}
+
+// console.log(minhaVariavel);
