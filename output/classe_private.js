ef4562ee711463ad623607a1dc386a94b795145a
VonexXT33 <josealv.neto04b@gmail.com>
2026-08-12 14:29:23 -0300

    Classe_private adicionado

--- output/classe_private.js
+++ output/classe_private.js
@@ -10,10 +10,41 @@ class Banco {
             return 'Valor indisponivel';
         }
     }
+    sacarDocaixa(quantidade) {
+        return this.debitarCofre(quantidade);
+    }
+    sacarDoCaixaEletronico(quantidade) {
+        return this.debitarCofre(quantidade);
+    }
 }
-class banco24Horas extends Banco {
-    sacarCaixaEletronico(quantidade) {
+class Banco24hrs extends Banco {
+    sacar(qntd) {
+        return this.sacarDocaixa(qntd);
     }
 }
 const nubank = new Banco();
+const nubank24hrs = new Banco24hrs();
+class Banco2 {
+    caixaforte = 20000;
+    sacarDoCaixaforte(qntd) {
+        if (this.caixaforte >= qntd) {
+            this.caixaforte -= qntd;
+            return this.caixaforte;
+        }
+        else {
+            return 'Valor indiponivel';
+        }
+    }
+    sacarBanco2(qntd) {
+        return this.sacarDoCaixaforte(qntd);
+    }
+}
+class Banco224hrs extends Banco2 {
+    cxEletronicoBanco2(qntd) {
+        return this.sacarBanco2(qntd);
+    }
+}
+const bradesco = new Banco2();
+const caixaBradesco = new Banco224hrs();
+console.log(bradesco.sacarBanco2(30000));
 //# sourceMappingURL=classe_private.js.map
--- scripts/classe_private.ts
+++ scripts/classe_private.ts
@@ -8,29 +8,103 @@ class Banco {
 
             return this.cofreQtd;
         }
+           else {
 
-        else {
-
-            return 'Valor indisponivel'
+        return 'Valor indisponivel';
 
         }
 
     }
+
+    protected sacarDocaixa (quantidade: number) {
+
+        return this.debitarCofre (quantidade);
+
+    }
+
+    public sacarDoCaixaEletronico (quantidade: number) {
+
+        return this.debitarCofre (quantidade);
+
+    } 
+
+
 }
 
-class banco24Horas extends Banco {
+class Banco24hrs extends Banco {
 
-   public sacarCaixaEletronico(quantidade: number) {
+   public sacar (qntd: number) {
+
+        return this.sacarDocaixa (qntd);
         
-        // return this.(quantidade);
-
-// Terminar classe sacarCaixaEletronico
-
     }
 
 }
 
 const nubank = new Banco();
+
+const nubank24hrs = new Banco24hrs();
+
+
+class Banco2 {
+
+    private caixaforte: number = 20000;
+    private sacarDoCaixaforte (qntd: number): number | string {
+
+        if (this.caixaforte >= qntd) {
+
+            this.caixaforte -= qntd;
+
+            return this.caixaforte;
+
+        }
+
+        else {
+
+            return 'Valor indiponivel';
+
+        }
+
+    }
+
+    public sacarBanco2 (qntd: number) {
+
+           return this.sacarDoCaixaforte(qntd);
+
+    }
+
+}
+
+class Banco224hrs extends Banco2 {
+
+    public cxEletronicoBanco2 (qntd: number) {
+
+              return this.sacarBanco2(qntd);
+
+    }     
+
+}
+
+const bradesco = new Banco2 ();
+
+const caixaBradesco = new Banco224hrs ();
+
+console.log(bradesco.sacarBanco2(30000));
+
+
+
+
+
+
+
+
+ 
+    
+
+
+
+
+
 
 
 
