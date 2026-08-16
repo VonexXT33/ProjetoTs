0c18e798a6b0495d86c528ee8b53230c405cfc21
VonexXT33 <josealv.neto04b@gmail.com>
2026-08-16 15:40:06 -0300

    Import e export da funcao mostrarMensagem

--- output/AtividadesTS/funcoes.js
+++ output/AtividadesTS/funcoes.js
@@ -3,8 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mensagem = (txt) => {
     console.log(txt);
 };
-mensagem('Olá, seja bem vindo!!');
-mensagem('Tudo bem?');
 const soma = (n1, n2) => {
     return n1 + n2;
 };
--- scripts/AtividadesTS/funcoes.ts
+++ scripts/AtividadesTS/funcoes.ts
@@ -1,14 +1,12 @@
 import {mostrarMensagem} from '../Funcoes/mostrarMensagem'
     
     const mensagem = (txt?: string): void => {
-    console.log(txt)
+      console.log(txt)
   
      }
-     mensagem('Olá, seja bem vindo!!');
-     mensagem('Tudo bem?')
-
+    
      const soma = (n1: number, n2: number): number => {
        return n1 + n2;
      }
 
- 
+    
